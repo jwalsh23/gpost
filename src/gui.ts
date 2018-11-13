61a8e3892d01e7a9ffa98c1a739bb0109292b144
jwalsh23 <jakewalsh1992@gmail.com>
2018-11-12 20:54:05 -0600

    adding back the original text to the html and removing old index.html -jw

--- src/gui.ts
+++ src/gui.ts
@@ -1,1 +1,5 @@
-document.getElementsByTagName('body')[0].innerHTML = `node Version: ${process.versions.node}`
+document.getElementsByTagName('body')[0].innerHTML = `<h1>Electron Application</h1>
+We are using node ${process.versions.node},
+Chrome ${process.versions.chrome},
+and Electron ${process.versions.electron}
+</body>`
