b4d71cf369b312ce878a7cb3d219f112c8d03e96
jwalsh23 <jakewalsh1992@gmail.com>
2018-11-12 18:53:49 -0600

    updated electron and added typescript file for the main -jw

--- main.js
+++ main.js
@@ -1,11 +1,10 @@
-const { app, BrowserWindow } = require('electron')
-  
-  function createWindow () {
+"use strict";
+exports.__esModule = true;
+var electron_1 = require("electron");
+function createWindow() {
     // Create the browser window.
-    win = new BrowserWindow({ width: 800, height: 600 })
-  
+    var win = new electron_1.BrowserWindow({ width: 800, height: 600 });
     // and load the index.html of the app.
-    win.loadFile('index.html')
-  }
-  
-  app.on('ready', createWindow)
+    win.loadFile('index.html');
+}
+electron_1.app.on('ready', createWindow);
